6b430bef0c4af4767b2de645d3df7ed22bb9d999
moonpxi <paulo.schneider@gmail.com>
2010-03-19 12:18:31 +0000

    Belvedere returns a fake list of views and users

--- test/MrBelvedere.rb
+++ test/MrBelvedere.rb
@@ -1,7 +1,37 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "test"))
 require "rubygems"
 require "sinatra"
+require "json"
+require "test_case_dsl"
 
-get "/" do
-  #"HowdyW"
-  ARGV
+configure do
+  Case = TestCase.load_from(ARGV[0])
+  StartTime = Time.now
+end
+
+helpers do
+  def render_json(json_map)
+    content_type :js, :charset => "UTF-8"
+    "#{params[:jsonp]}(#{json_map.to_json})"
+  end
+end
+
+before do
+ time_since_start = (Time.now - StartTime).round
+ Case.update_projects_at(time_since_start)
+end
+
+# Get list of views
+get "/api/json" do
+  render_json :views => [{:name => "Default"}]
+end
+
+# Get list of users
+get "/people/api/json" do
+  render_json :users => [{:user => {:fullName => "default_user"}}]
+end
+
+# Get failed jobs
+get "/view/:view_name/api/json" do
 end
--- test/testcase1.rb
+++ test/testcase1.rb
@@ -4,10 +4,10 @@ setup do
 end
 
 events do
-  after 60  { change("ProjectA", :status => :started) }
-  after 70  { change("ProjectA", :status => :failed) }
-  after 90  { change("ProjectB", :status => :started) }
-  after 110 { change("ProjectB", :status => :unstable) }
-  after 120 { change("ProjectA", :status => :started) }
-  after 150 { change("ProjectA", :status => :passed) }
+  after(60)  { change("ProjectA", :status => :started) }
+  after(70)  { change("ProjectA", :status => :failed) }
+  after(90)  { change("ProjectB", :status => :started) }
+  after(110) { change("ProjectB", :status => :unstable) }
+  after(120) { change("ProjectA", :status => :started) }
+  after(150) { change("ProjectA", :status => :passed) }
 end
